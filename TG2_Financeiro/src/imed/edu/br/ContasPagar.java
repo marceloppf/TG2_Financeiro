4d3e1d2f8e6b63fdc719b1cdfa47409472dec47f
1119714 <1119714@imed.edu.br>
2019-11-12 19:05:53 -0300

    Criado Atributos para Contas Receber e Contas Pagar

--- TG2_Financeiro/src/imed/edu/br/ContasPagar.java
+++ TG2_Financeiro/src/imed/edu/br/ContasPagar.java
@@ -1,5 +1,8 @@
 package imed.edu.br;
 
 public class ContasPagar {
-
+	//Caixa Id_Doc;
+	String Dt_PG;
+	Double Vlr_PG;
+	String Status_CP;
 }
--- TG2_Financeiro/src/imed/edu/br/ContasReceber.java
+++ TG2_Financeiro/src/imed/edu/br/ContasReceber.java
@@ -1,5 +1,10 @@
 package imed.edu.br;
 
 public class ContasReceber {
-
+	//Caixa Id_Doc;
+	String Dt_Receb;
+	Double Vlr_Receb;
+	String Status_CR;
+	
+	
 }
